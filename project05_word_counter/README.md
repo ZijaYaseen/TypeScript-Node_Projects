cff132fdf845ffbf5f02f4aa3dcac2359c8a6e3e
Zija Yaseen <zijayaseen15@gmail.com>
2024-04-01 14:51:48 -0800

    add new project

---
+++ project05_word_counter/README.md
@@ -0,0 +1,5 @@
+WOrd counter app;
+It counts words of your provided text.
+
+use it by yourself.
+npx command: npx z15-word-counter
---
+++ project05_word_counter/app.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+    let userAns = await inquirer.prompt(
+        {
+            name : "sentence",
+            type : "input",
+            message : "Enter your sentence to count the words: \n"
+        }
+    )
+
+    if(userAns.sentence){
+        let splittingWords = userAns.sentence.trim().split(" ");
+        let countingWords = splittingWords.length;
+        console.log(`Your words in this sentence are: ${chalk.bold.cyan(countingWords)}`);
+    }
+    else{
+        console.log(chalk.bold.red(`Please Enter a Sentence!!`));
+        
+    }
